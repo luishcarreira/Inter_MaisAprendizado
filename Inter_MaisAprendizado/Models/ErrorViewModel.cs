6a7bdfa342a9ac77ba51a2c755813b4e3893a309
Luis Henrique Carreira <56508446+luishcarreira@users.noreply.github.com>
2020-10-05 19:39:50 -0300

    Adicionar arquivos de projeto.

---
+++ Inter_MaisAprendizado/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Inter_MaisAprendizado.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Inter_MaisAprendizado/Models/Pessoa.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Inter_MaisAprendizado.Models
+{
+    abstract class Pessoa
+    {
+        public int IdPessoa { get; set; }
+
+        [Display(Name = "Nome")]
+        [MinLength(3)]
+        public string Nome { get; set; }
+
+        [Display(Name = "Email")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Display(Name = "Data Nascimento")]
+        [DataType(DataType.Date)]
+        public DateTime DataNasc { get; set; }
+
+        public Pessoa()
+        {
+            IdPessoa = 0;
+            Nome = null;
+            Email = null;
+            DataNasc = DateTime.Now;
+        }
+    }
+}
