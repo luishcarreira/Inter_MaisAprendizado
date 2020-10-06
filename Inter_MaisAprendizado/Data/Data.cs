584e3892821a5af56d8171f04aca60cadc2c6322
Luis Henrique Carreira <56508446+luishcarreira@users.noreply.github.com>
2020-10-06 10:41:55 -0300

    Classe Data

---
+++ Inter_MaisAprendizado/Data/Data.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Inter_MaisAprendizado.Data
+{
+    public class Data : IDisposable
+    {
+        //Atributo: vai nos permitir conectar com Banco de Dados
+        protected SqlConnection connectionDB;
+
+        public Data()
+        {
+            try
+            {
+                //string de conexão com o banco de dados
+                string strConexao = @"Data Source=DESKTOP-I1FBU43\SQLEXPRESS;Initial Catalog=;Integrated Security=true";
+
+                connectionDB = new SqlConnection(strConexao);
+                connectionDB.Open();
+            }
+            catch (SqlException er)
+            {
+                Console.WriteLine("Erro de banco " + er);
+            }
+        }
+
+        public void Dispose()
+        {
+            connectionDB.Close();
+        }
+    }
+}
